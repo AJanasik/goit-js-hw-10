4f3636abfc6e3282d3a224625fddd556389346f3
AJanasik <ajanasik1@gmail.com>
2023-05-05 21:04:09 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,17 +2,13 @@ export { fetchCountries };
 import Notiflix from 'notiflix';
 
 async function fetchCountries(name) {
-  return await fetch(
+  const response = await fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (response.status === 404) {
-        throw new Error(404);
-      }
+  );
+  const data = await response.json();
 
-      return response.json();
-    })
-    .catch(error =>
-      Notiflix.Notify.failure('Oops, there is no country with that name')
-    );
+  if (response.status === 404) {
+    throw new Error('Oops, there is no country with that name');
+  }
+  return data;
 }
